be19706518674d9133da45c49adc4f8b8da910fe
Enric Armengol <enarga@jviladoms.cat>
2025-02-12 00:53:22 +0100

    [FIX] removed my name duh

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,14 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "PokédexEnric",
+  title: "PokédexKuwuro",
   description: "Pokédex for the BLING Technical Test",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <link
-        rel="icon"
-        href="/poke.png"
-        type="image/png"
-        sizes="<generated>"
-      />
+    <html lang='en'>
+      <link rel='icon' href='/poke.png' type='image/png' sizes='<generated>' />
       <body>{children}</body>
     </html>
   );
--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { useState, useRef } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const router = useRouter();
   const searchRef = useRef(null);
 
@@ -15,31 +15,34 @@ export default function SearchBar() {
     } else {
       const inputElement = searchRef.current;
       if (inputElement) {
-        inputElement.classList.add('animate-shake', 'border-red-500');
+        inputElement.classList.add("animate-shake", "border-red-500");
         inputElement.focus();
         setTimeout(() => {
-          inputElement.classList.remove('animate-shake', 'border-red-500');
+          inputElement.classList.remove("animate-shake", "border-red-500");
         }, 800);
       }
     }
   };
 
   return (
-    <div className='sticky top-0 flex flex-col gap-3 lg:pt-12 pt-10 pb-6 lg:w-6/12 w-auto justify-center items-center bg-[#FEDCC8] z-10 animate-fade-in'>        
-      <h2 className="text-4xl font-bold text-center">PokédexEnric</h2>
-      <form onSubmit={handleSubmit} className="flex gap-2 w-9/12 mb-6 justify-center items-center">
+    <div className='sticky top-0 flex flex-col gap-3 lg:pt-12 pt-10 pb-6 lg:w-6/12 w-auto justify-center items-center bg-[#FEDCC8] z-10 animate-fade-in'>
+      <h2 className='text-4xl font-bold text-center'>PokédexKuwuro</h2>
+      <form
+        onSubmit={handleSubmit}
+        className='flex gap-2 w-9/12 mb-6 justify-center items-center'
+      >
         <input
-          id="search"
-          type="text"
+          id='search'
+          type='text'
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for a Pokémon..."
-          className="border border-gray-300 p-2 rounded-md w-80 transition-colors duration-200 focus:outline-none"
+          placeholder='Search for a Pokémon...'
+          className='border border-gray-300 p-2 rounded-md w-80 transition-colors duration-200 focus:outline-none'
           ref={searchRef}
         />
         <button
-          type="submit"
-          className="bg-blue-500 lg:hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors duration-200"
+          type='submit'
+          className='bg-blue-500 lg:hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors duration-200'
         >
           Search
         </button>
